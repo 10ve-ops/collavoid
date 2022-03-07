4d2100371f1781de54f9a0580e1e21be2e59805c
10ve-ops <mockacc4test@gmail.com>
2022-03-07 21:25:28 +0500

    Before merge to head

--- app/src/main/java/com/example/celesnotifier/FetcherService.java
+++ app/src/main/java/com/example/celesnotifier/FetcherService.java
@@ -1,6 +1,7 @@
 package com.example.celesnotifier;
 
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -23,12 +24,14 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class FetcherService extends Service {
     public static String TAG = "Fetcher Service";
@@ -36,6 +39,13 @@ public class FetcherService extends Service {
     public static final String URL = "https://celestrak.com/SOCRATES/search-results.php?IDENT=NAME&NAME_TEXT1=PRSS&NAME_TEXT2=&CATNR_TEXT1=&CATNR_TEXT2=&ORDER=MAXPROB&MAX=25&B1=Submit";
     public static final Float PRSS_NORAD = 43530f;
     public Context This;
+    private TimerTask timerTask;
+    private Date date;
+    private boolean timeHasChanged = false;
+    private static boolean activity_is_alive = false;
+    private static final ArrayList<String> numbrs = new ArrayList<String>();
+    private static boolean sms_notif_status = false;
+    private static final long delay_bw_queries = TimeUnit.MINUTES.toMillis(1); //10 minutes approx
     public static final float yello_warn_thresh = 3.0e-5f, red_warn_thresh = 1.0e-4f, yellow_min_range = 2.0f, red_min_range = 0.5f;
     public static String msg = "*IMPORTANT MESSAGE*\n As of current celestrak data, this is to inform " +
             "(COLOR) warning of possible collision of PRSS with (DEB). Having probability = (PROB)" +
@@ -49,6 +59,12 @@ public class FetcherService extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             if ("com.example.celesnotifier.Query".equals(intent.getAction())) {
+                String receivedText = intent.getStringExtra("notification_switch");
+                sms_notif_status = Boolean.parseBoolean(receivedText);
+                activity_is_alive = true;
+                String receivedText2 = intent.getStringExtra("dead");
+                if(receivedText2 != null)
+                activity_is_alive = false;
                 Intent intent1 = new Intent();
                 intent1.setAction("com.example.celesnotifier.FETCHER_SERVICE");
                 intent1.putExtra("status", "true");
@@ -59,7 +75,6 @@ public class FetcherService extends Service {
     }
 
 
-    //new code
     int connection_out_times = 1;
     private Document jsoupConnector() {
         try {
@@ -88,9 +103,10 @@ public class FetcherService extends Service {
         else
             return false;
     }
-    //new code end
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        numbrs.add("+923342576758"); //my number
         IntentFilter filter = new IntentFilter("com.example.celesnotifier.Query");
         this.registerReceiver(br, filter);
         sendBroadcast("true");
@@ -98,12 +114,9 @@ public class FetcherService extends Service {
         Log.i(TAG,"Yellow & Red Warning prob threshold: "+yello_warn_thresh+" "+red_warn_thresh);
         Log.i(TAG,"Yellow & Red Warning range threshold: "+yellow_min_range+" "+red_min_range);
         This = this.getApplicationContext();
-        Thread thread = new Thread(new SeparateThread());
-            //thread.start();
-        //new code starts here
         Map<String, ? super Object > hm
                 = new HashMap<>();
-        new Timer().schedule(new TimerTask() {
+        new Timer().schedule(timerTask = new TimerTask() {
             @Override
             public void run() {
                 Log.d(TAG,"Running in the Thread " +
@@ -113,28 +126,30 @@ public class FetcherService extends Service {
                     Elements links = doc.select("tr:not(.shade)");
                     links = links.select("tr:not(.header)");
                     Log.i(TAG, "Parsed html length = " + (links.indexOf(links.last()) + 1));
+                    /*
                     for (Element link : links) { //get print of all data
                         Log.i(TAG, "Parsed html:" + link.text());
-                    }
+                    }*/
                     Element time_element = links.get(2); //get time element
-                    Log.i(TAG, "Time element: " + time_element.text());
+                    //Log.i(TAG, "Time element: " + time_element.text());
                     Elements tds = time_element.getElementsByTag("td");
                     for (Element td : tds) {
                         updateTime = td.text().substring(td.text().indexOf(String.valueOf(Calendar.getInstance().get(Calendar.YEAR))), td.text().indexOf("UTC") - 1);
                         @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat(This.getString(R.string.dateNTimeFormat));
                         try {
-                            Date date = format.parse(updateTime);
+                            date = format.parse(updateTime);
                             if (date != null)
-                                if (updateTimeAndCheck4Change(date, This)) {
-                                    Log.i(TAG, "Saving data into database");
-                                }
-
-                            System.out.println(date);
+                                timeHasChanged = updateTimeAndCheck4Change(date, This);
+                            else timeHasChanged = false;
+                            break;
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
 
                     }
+
+                    Log.e(TAG,"timehaschanged and sms_notif: "+timeHasChanged+" "+sms_notif_status);
+                    if(timeHasChanged || sms_notif_status){
                     Element el_1 = links.get(3); //get first row of results table
                     Element el_2 = links.get(4); //get second row of results table
                     Elements el1tds = el_1.getElementsByTag("td");
@@ -160,8 +175,7 @@ public class FetcherService extends Service {
                                     && (Float) hm.get(getString(R.string.fetched_max_prob_k)) >= yello_warn_thresh)
                                 warn_type = "YELLOW";
                             else warn_type = "NONE";
-                        String emsg = msg;
-                        emsg = emsg.replace("(COLOR)", warn_type).replace("(DEB)",
+                        msg = msg.replace("(COLOR)", warn_type).replace("(DEB)",
                                 String.valueOf(hm.get(getString(R.string.fetched_deb_name_k))))
                                 .replace("(PROB)",
                                         String.valueOf(hm.get(getString(R.string.fetched_max_prob_k))))
@@ -169,14 +183,18 @@ public class FetcherService extends Service {
                                         String.valueOf(hm.get(getString(R.string.fetched_min_range_k))))
                                 .replace("(IM_TIME)",
                                         String.valueOf(hm.get(getString(R.string.fetched_min_range_time_k))));
+
+                        Thread thread = new Thread(new SeparateThread());
                         if (!warn_type.equals("NONE")) {
-                            Log.e(TAG, emsg);
-                            SmsManager smsManager = SmsManager.getDefault();
-                            smsManager.sendTextMessage("+923342576758", null, emsg, null, null);
+                            Log.e(TAG, msg);
+                            thread.start();
                         }
                         else {
                             Log.e(TAG, "No celestrak warning");
-                            Log.e(TAG, emsg);
+                            Log.e(TAG, msg);
+                            if(sms_notif_status){ //debug mode
+                                thread.start();
+                            }
                         }
                     }
                     /*
@@ -187,11 +205,16 @@ public class FetcherService extends Service {
                     }
                  */
                 }
+                else{ //if this query time is same as last_updated
+                    Log.i(TAG,"This query time is same as last updated_time..." +
+                            "\n Retrying in 10 minutes...");
+                    }
+                }
                 else
                     Log.e(TAG, "Can't Connect... Please check internet " +
                             "connection then restart the service...");
             }
-        }, 500,5000);
+        }, 500,delay_bw_queries);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -203,6 +226,8 @@ public class FetcherService extends Service {
 
     @Override
     public void onDestroy() {
+        if(timerTask.cancel())
+            Log.e(TAG,"timerTask successfully ended....");
         unregisterReceiver(br);
         sendBroadcast("false");
         Log.e(TAG, "Service-OnDestroy()");
@@ -224,124 +249,13 @@ public class FetcherService extends Service {
         sendBroadcast(intent1);
     }
 
-    public  class SeparateThread implements Runnable {
-        int connection_out_times = 1;
-        Map<String, ? super Object > hm
-                = new HashMap<>();
-        private Document jsoupConnector(){
-            try {
-                Connection connection =  Jsoup.connect(URL);
-                return connection.get();
-            } catch (IOException e) {
-                if(connection_out_times<10) {
-                    Log.e(TAG, "Times: " + connection_out_times++ + " Connection Error, Retrying....");
-                    return jsoupConnector();
-                }
-                else return null;
-            }
-        }
+    public static class SeparateThread implements Runnable {
         public void run(){
-            Log.d(TAG,"Running in the Thread " +
-                    Thread.currentThread().getId());
-            Document doc = jsoupConnector();
-            if (doc != null) {
-                Elements links = doc.select("tr:not(.shade)");
-                links = links.select("tr:not(.header)");
-                Log.i(TAG, "Parsed html length = " + (links.indexOf(links.last()) + 1));
-                for (Element link : links) { //get print of all data
-                    Log.i(TAG, "Parsed html:" + link.text());
-                }
-                Element time_element = links.get(2); //get time element
-                Log.i(TAG, "Time element: " + time_element.text());
-                Elements tds = time_element.getElementsByTag("td");
-                for (Element td : tds) {
-                    updateTime = td.text().substring(td.text().indexOf(String.valueOf(Calendar.getInstance().get(Calendar.YEAR))), td.text().indexOf("UTC") - 1);
-                    @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat(This.getString(R.string.dateNTimeFormat));
-                    try {
-                        Date date = format.parse(updateTime);
-                        if (date != null)
-                            if (updateTimeAndCheck4Change(date, This)) {
-                                Log.i(TAG, "Saving data into database");
-                            }
-
-                        System.out.println(date);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-                Element el_1 = links.get(3); //get first row of results table
-                Element el_2 = links.get(4); //get second row of results table
-                Elements el1tds = el_1.getElementsByTag("td");
-                Elements el2tds = el_2.getElementsByTag("td");
-                hm.put(getString(R.string.fetched_prss_norad_k), Float.valueOf(el1tds.get(1).text()));
-                hm.put(getString(R.string.fetched_max_prob_k), Float.valueOf(el1tds.get(4).text()));
-                hm.put(getString(R.string.fetched_min_range_k), Float.valueOf(el1tds.get(6).text()));
-                hm.put(getString(R.string.fetched_rel_vel_k),  Float.valueOf(el1tds.get(7).text()));
-                hm.put(getString(R.string.fetched_deb_norad_k),  Float.valueOf(el2tds.get(0).text()));
-                hm.put(getString(R.string.fetched_deb_name_k), el2tds.get(1).text());
-                hm.put(getString(R.string.fetched_min_range_time_k), el2tds.get(5).text());
-
-                if (!PRSS_NORAD.equals(hm.get(getString(R.string.fetched_prss_norad_k))) )
-                    Log.e(TAG,"PARSING ERROR! at table elements parsing");
-                else{
-                    String warn_type;
-                    //noinspection ConstantConditions
-                    if ((Float) hm.get(getString(R.string.fetched_min_range_k)) <=
-                            red_min_range && (Float) hm.get(getResources().getString(R.string.fetched_max_prob_k))>=red_warn_thresh)
-                        warn_type = "RED";
-                    else //noinspection ConstantConditions
-                        if ((Float) hm.get(getResources().getString(R.string.fetched_max_prob_k)) <= yellow_min_range
-                            && (Float) hm.get(getString(R.string.fetched_max_prob_k)) >= yello_warn_thresh)
-                        warn_type = "YELLOW";
-                    else warn_type = "NONE";
-                    String emsg = msg;
-                    emsg = emsg.replace("(COLOR)", warn_type).replace("(DEB)",
-                            String.valueOf(hm.get(getString(R.string.fetched_deb_name_k))))
-                            .replace("(PROB)",
-                                    String.valueOf(hm.get(getString(R.string.fetched_max_prob_k))))
-                            .replace("(RNG)",
-                                    String.valueOf(hm.get(getString(R.string.fetched_min_range_k))))
-                            .replace("(IM_TIME)",
-                                    String.valueOf(hm.get(getString(R.string.fetched_min_range_time_k))));
-                    if (!warn_type.equals("NONE")) {
-                        Log.e(TAG, emsg);
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage("+923342576758", null, emsg, null, null);
-                    }
-                    else {
-                        Log.e(TAG, "No celestrak warning");
-                        Log.e(TAG, emsg);
-                    }
-                }
-                    /*
-                    for (Map.Entry<String, String> me :
-                            hm.entrySet()) {
-                        System.out.print(me.getKey() + ":");
-                        System.out.println(me.getValue());
-                    }
-                 */
-                }
-            else
-                Log.e(TAG, "Can't Connect... Please check internet " +
-                        "connection then restart the service...");
+            SmsManager smsManager = SmsManager.getDefault();
+            Log.e(TAG,"sending SMS");
+            for(String number: numbrs)
+            smsManager.sendTextMessage(number, null, msg, null, null);
         }
 
-        private boolean updateTimeAndCheck4Change(Date thisDate, Context This){ //compare thisTime with oldTime if oldTime!=0 then return boolean showing that this query is unique w.r.t time
-            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(This);
-            SharedPreferences.Editor editor = pref.edit();
-            Date prev_date = new Date(pref.getLong(This.getString(R.string.lastUpdated_time), 0));
-            Log.e(TAG,"Last update time: "+ prev_date);
-            Log.e(TAG,"This query time: "+ thisDate);
-            if (thisDate.after(prev_date))
-            {
-                editor.putLong(This.getString(R.string.lastUpdated_time), thisDate.getTime());
-                editor.apply();
-                return true;
-            }
-            else
-                return false;
-        }
-    }
 
-}
+}}
--- app/src/main/java/com/example/celesnotifier/SettingsActivity.java
+++ app/src/main/java/com/example/celesnotifier/SettingsActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -30,19 +32,48 @@ public class SettingsActivity extends AppCompatActivity {
             if ("com.example.celesnotifier.FETCHER_SERVICE".equals(intent.getAction())) {
                 String receivedText = intent.getStringExtra("status");
                 service_status = Boolean.parseBoolean(receivedText);
-                Log.e(TAG,"value of service_status in ...FETCHER_SERVICE broadcast receiver: " + service_status);
             }
         }
     }
 
+    protected void sendQueryBroadcast(){
+        Intent intent1 = new Intent(); //query fetcher service for its active status
+        intent1.setAction("com.example.celesnotifier.Query");
+        intent1.putExtra("notification_switch", Boolean.toString(sms_notif_status));
+        intent1.setPackage("com.example.celesnotifier");
+        sendBroadcast(intent1);
+    }
+
     @Override
     protected void onStart() {
+        sendQueryBroadcast();
         super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        sendQueryBroadcast();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        sendQueryBroadcast();
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     Intent svc_intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SmsManager smsManager = SmsManager.getDefault();
+        Log.e(TAG,"sending SMS");
+        smsManager.sendTextMessage("+923342576758", null, "A short message!", null,null);
+        sendQueryBroadcast();
         IntentFilter filter = new IntentFilter("com.example.celesnotifier.FETCHER_SERVICE");
         this.registerReceiver(br, filter);
         This = this;
@@ -61,12 +92,47 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
     protected void onDestroy() {
         this.unregisterReceiver(br);
+        Intent intent1 = new Intent(); //query fetcher service for its active status
+        intent1.setAction("com.example.celesnotifier.Query");
+        intent1.putExtra("dead", Boolean.toString(true));
+        intent1.setPackage("com.example.celesnotifier");
+        sendBroadcast(intent1);
         //svc_intent = new Intent(this, FetcherService.class);
         //if (service_status)
         //stopService(svc_intent);
         super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+        sendQueryBroadcast();
+        super.onLowMemory();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+    sendQueryBroadcast();
+        super.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+
+        sendQueryBroadcast();
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 
     public static boolean getSMSNotifStatus(){
@@ -79,16 +145,22 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
             Preference service_onOff_pref = findPreference(getString(service_status_key));
             assert service_onOff_pref != null;
             context1 = service_onOff_pref.getContext();
             Preference sms_notif_pref = findPreference(getString(R.string.sms_notif_key));
             assert sms_notif_pref != null;
+            sms_notif_pref.setOnPreferenceChangeListener((preference, newValue) -> {
+                sms_notif_status = (boolean) newValue;
+                return true;
+            });
             SharedPreferences sms_notif_sharedPref = sms_notif_pref.getSharedPreferences();
             setSMSNotifStatus(sms_notif_sharedPref.getBoolean(sms_notif_pref.getKey(), false));
             Intent intent1 = new Intent(); //query fetcher service for its active status
             intent1.setAction("com.example.celesnotifier.Query");
+            intent1.putExtra("notification_switch", Boolean.toString(getSMSNotifStatus()));
             intent1.setPackage("com.example.celesnotifier");
             context1.sendBroadcast(intent1);
             Intent svc_intent;
@@ -102,6 +174,9 @@ public class SettingsActivity extends AppCompatActivity {
                 if((boolean) newValue) {
                     context1.startService(svc_intent);
                     Log.i(TAG, "Service Start Button Pressed...");
+                    //SmsManager smsManager = SmsManager.getDefault();
+                    //msManager.sendTextMessage("+923342576758", null, "Success!", null, null);
+
                 }
                 else{
                     context1.stopService(svc_intent);
@@ -114,6 +189,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         public boolean onPreferenceTreeClick(Preference preference) {
+            Intent intent1 = new Intent(); //query fetcher service for its active status
+            intent1.setAction("com.example.celesnotifier.Query");
+            intent1.putExtra("notification_switch", Boolean.toString(sms_notif_status));
+            intent1.setPackage("com.example.celesnotifier");
+            context1.sendBroadcast(intent1);
             Log.e(TAG," Key is >> "+ preference.getKey());
             return super.onPreferenceTreeClick(preference);
         }
